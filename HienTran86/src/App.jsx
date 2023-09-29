2d794b32954358af2b91f1038227e626e939efb2
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2023-09-29 17:35:06 +0700

    6

--- HienTran86/src/App.jsx
+++ HienTran86/src/App.jsx
@@ -5,13 +5,13 @@ import style from './App.module.css'
 import { AppContainer } from "./style";
 import Image from './assets/react.svg';
 
-// eslint-disable-next-line no-unused-vars
+
 function App() {
   
-    // Bước 2: Khai báo state time và giá trị mặc định là 'day'
+   
     const [time, setTime] = useState('day');
   
-    // Bước 3: Khai báo hàm onClick cho mỗi button để cập nhật state time
+   
     const handleTimeChange = (newTime) => {
       setTime(newTime);
     };
@@ -20,7 +20,7 @@ function App() {
       <div className="App">
         <button onClick={() => handleTimeChange('night')}>Night</button>
         <button onClick={() => handleTimeChange('day')}>Day</button>
-        {/* Bước 4: Tạo phần tử div thay đổi màu theo state time */}
+      
         <div
           style={{
             width: '200px',
@@ -28,7 +28,7 @@ function App() {
             backgroundColor: time === 'day' ? 'blue' : 'black',
           }}
         >
-          {/* Nội dung bên trong div */}
+          
         </div>
       </div>
     );
